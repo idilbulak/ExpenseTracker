4d29d23dd0c6c6861ae8be32a2c5dfa5aab75e75
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-09-27 01:36:15 +0200

    views done

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
+use App\Config\Paths;
 
 class HomeController {
 
     private TemplateEngine $view;
 
     public function __construct() {
-        $this->view = new TemplateEngine();
+        $this->view = new TemplateEngine(Paths::VIEW);
     }
 
     public function home() {
-        dd($this->view);
-        echo "HomePage";
+        $this->view->render("/index.php");
     }
 }
--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -5,5 +5,13 @@ declare(strict_types=1);
 namespace Framework;
 
 class TemplateEngine {
+    private string $basePath;
 
+    public function __construct(string $basePath){
+        $this->basePath = $basePath;
+    }
+
+    public function render(string $template) {
+        include "{$this->basePath}/{$template}";
+    }
 }
