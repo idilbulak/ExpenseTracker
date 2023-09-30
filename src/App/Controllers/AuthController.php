89f3e0a2ee0aec725eaabda50f548c4714768cee
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-09-30 14:55:22 +0200

    validator service injected

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -6,13 +6,16 @@ namespace App\Controllers;
 
 use Framework\TemplateEngine;
 use App\Config\Paths;
+use App\Services\ValidatorService;
 
 class AuthController {
 
     private TemplateEngine $view;
+    private ValidatorService $validatorService;
 
-    public function __construct(TemplateEngine $view) {
+    public function __construct(TemplateEngine $view, ValidatorService $validatorService) {
         $this->view = $view;
+        $this->validatorService = $validatorService;
     }
 
     public function registerView() {
