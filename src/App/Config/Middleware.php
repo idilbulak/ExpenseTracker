895a5100df8c5a090f52e1225d29dab4f300e113
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-09-30 21:51:18 +0200

    rules

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -8,11 +8,18 @@ use Framework\App;
 use App\Middleware\{
     TemplateDataMiddleware,
     ValidationExceptionMiddleware,
-    SessionMiddleware
+    SessionMiddleware,
+    FlashMiddleware
+//    CsrfTokenMiddleware,
+//    CsrfGuardMiddleware
 };
 
-function registerMiddleware(App $app) {
+function registerMiddleware(App $app)
+{
+//    $app->addMiddleware(CsrfGuardMiddleware::class);
+//    $app->addMiddleware(CsrfTokenMiddleware::class);
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
+    $app->addMiddleware(FlashMiddleware::class);
     $app->addMiddleware(SessionMiddleware::class);
 }
--- src/Framework/Contracts/RuleInterface.php
+++ src/Framework/Contracts/RuleInterface.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Framework\Contracts;
 
-interface RuleInterface {
-    public function validate(array $data, string $field, array $params) : bool;
+interface RuleInterface
+{
+    public function validate(array $data, string $field, array $params): bool;
 
     public function getMessage(array $data, string $field, array $params): string;
 }
