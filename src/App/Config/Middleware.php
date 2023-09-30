2f7eb04f9f5c968d41ea8532485aad7862d859cf
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-09-30 20:59:07 +0200

    sessions

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -7,10 +7,12 @@ namespace App\Config;
 use Framework\App;
 use App\Middleware\{
     TemplateDataMiddleware,
-    ValidationExceptionMiddleware
+    ValidationExceptionMiddleware,
+    SessionMiddleware
 };
 
 function registerMiddleware(App $app) {
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
+    $app->addMiddleware(SessionMiddleware::class);
 }
